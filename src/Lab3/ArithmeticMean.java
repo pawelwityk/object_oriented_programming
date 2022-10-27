8b75f3b228823c44dcbd2db8cf2440e56ce428ea
pwityk <pawelwityk@student.agh.edu.pl>
2022-10-27 11:14:50 +0200

    some changes in ArithmeticMean

--- src/Lab3/ArithmeticMean.java
+++ src/Lab3/ArithmeticMean.java
@@ -1,20 +1,17 @@
 package Lab3;
-
-import java.util.ArrayList;
-import java.util.List;
 public class ArithmeticMean {
     public static void main(String[] args) {
         if (args.length == 0){
             return;
         }
         try {
-            List<String> numbers = new ArrayList<String>();
-            int sum = 0;
-            for (int i = 0; i < args.length; i++) {
+            int sum = Integer.parseInt(args[0]);
+            System.out.printf("Średnia arytmetyczna liczb: " + sum);
+            for (int i = 1; i < args.length; i++) {
                 sum += Integer.parseInt(args[i]);
-                numbers.add(args[i]);
+                System.out.printf(", " + args[i]);
             }
-            System.out.println("Średnia arytmetyczna liczb " + numbers + " wynosi " + (sum % args.length == 0 ? sum/args.length : sum/args.length + ", reszta " + sum % args.length));
+            System.out.printf(" wynosi: " + (sum % args.length == 0 ? sum/args.length : sum/args.length + ", reszta: " + sum % args.length));
         }
         catch(NumberFormatException exception){
             System.out.println("Zostały wprowadzone znaki zamiast cyfr !");
