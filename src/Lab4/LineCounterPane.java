1aa42f1ecbe1ae80d0a97ba1f1d01acf20f288e4
pwityk <pawelwityk@student.agh.edu.pl>
2022-11-01 19:03:20 +0100

    new task solved

--- src/Lab4/LineCounterPane.java
+++ src/Lab4/LineCounterPane.java
@@ -1,4 +1,31 @@
 package Lab4;
 
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.FileReader;
+import javax.swing.JOptionPane;
+
+
 public class LineCounterPane {
+    public static void main(String[] args){
+        try{
+            String fileToRead = JOptionPane.showInputDialog("Podaj nazwę pliku:");
+            System.out.println(fileToRead);
+            FileReader file = new FileReader(fileToRead); // reading file
+            Scanner readFile = new Scanner(file); //
+            int i = 0;
+            while(readFile.hasNextLine()){
+                readFile.nextLine();
+                i++;
+            }
+            System.out.println("Liczba wierszy w pliku " + fileToRead +  " wynosi: " + i);
+
+        }
+        catch(FileNotFoundException exception){
+            System.out.println("Plik nie istnieje !");
+        }
+        catch(ArrayIndexOutOfBoundsException exception){
+            System.out.println("Nie podano pliku, bądź podano kilka plików naraz !");
+        }
+    }
 }
