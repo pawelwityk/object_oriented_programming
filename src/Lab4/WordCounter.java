a33c04394baa1a4c7604d8b77799efcbd87a1c21
pawelwityk <114407180+pawelwityk@users.noreply.github.com>
2022-11-10 11:05:03 +0100

    Update WordCounter.java

--- src/Lab4/WordCounter.java
+++ src/Lab4/WordCounter.java
@@ -1,91 +1,90 @@
 package Lab4;
 
-import java.io.*;
-import java.nio.file.AccessDeniedException;
-import java.net.*;
-import java.nio.file.Paths;
-import java.nio.file.Files;
-import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
 
-public class FileAndURLCopy{
-    public static void main(String[] args) throws IOException {
-        if (args.length < 2) {
-            System.out.println("Za mało argumentów wywołania programu !\n" +  "Przykład użycia: 'java FileCopy <żródło> <cel>'.");
+public class WordCounter {
+    private static String getArgs(String[] args) {
+        StringBuilder argString = new StringBuilder();
+        for (String arg : args) {
+            if (!arg.startsWith("-")) continue;
+
+            for (int i = 1; i < arg.length(); ++i) {
+                argString.append(arg.charAt(i));
+            }
+        }
+        return argString.toString();
+    }
+
+    private static String getFilename(String[] args) {
+        for (String arg : args) {
+            if (!arg.startsWith("-")) {
+                return arg;
+            }
+        }
+        return null;
+    }
+
+    private static int[] getData(FileReader file) throws IOException {
+        BufferedReader readFile = new BufferedReader(file);
+        int lineCount = 0, charCount = 0, wordCount = 0;
+        String line;
+
+        while((line = readFile.readLine()) != null) {
+            lineCount++;
+            wordCount += line.split("\\s+").length;
+            charCount += line.length();
+        }
+        return new int[]{lineCount, charCount, wordCount};
+    }
+    public static void main(String[] args) {
+        FileReader file;
+        int[] result = null;
+        String filename = getFilename(args);
+        if (filename == null) {
+            System.out.println("Nie podano pliku !");
+            System.exit(1);
+        }
+        try{
+            file = new FileReader(filename); // reading file
+            result = getData(file);
+        }
+        catch(FileNotFoundException exception){
+            System.out.println("Plik nie istnieje !");
+            System.exit(1);
+        }
+        catch(ArrayIndexOutOfBoundsException exception){
+            System.out.println("Nie podano pliku, bądź podano kilka plików naraz !");
+            System.exit(1);
+        }
+        catch(IOException e){
+            System.out.println("Błąd IO !");
             System.exit(1);
         }
 
-        if (args[0].startsWith("http")) {
+        String arguments = getArgs(args);
 
-            URLConnection conn;
-            InputStream in1 = null;
-            URL url = null;
+        boolean isL = arguments.contains("l");
+        boolean isC = arguments.contains("c");
+        boolean isW = arguments.contains("w");
 
-            try {
-                url = new URL(args[0]);
-                conn = url.openConnection();
-                in1 = conn.getInputStream();
-            }
-            catch(UnknownHostException e) {
-                System.out.println("Podany adres " + url + " jest nieprawidłowy");
-                System.exit(1);
-            }
-            catch(AccessDeniedException e) {
-                System.out.println("Brak dostępu do " + url);
-                System.exit(1);
-            }
-
-            try {
-                FileOutputStream dstFile = new FileOutputStream(args[1]);
-                byte[] array = in1.readAllBytes();
-                dstFile.write(array);
-                dstFile.close();
-            }
-            catch(IOException e) {
-                System.out.println("Błąd kopiowania");
-                System.exit(1);
-            }
-
+        if(isL) {
+            System.out.println("wierszy: "  + result[0]);
         }
-        else {
-
-            File srcFile = new File(args[0]);
-            File dstFile = new File(args[1]);
-
-            if (!srcFile.exists()) {
-                System.out.println("Plik " + srcFile.getName() + " nie istnieje !");
-                System.exit(1);
-            }
-            if (srcFile.isDirectory()) {
-                System.out.println(srcFile.getName() + " jest katalogiem !");
-                System.exit(1);
-            }
-            if (!srcFile.canRead()) {
-                System.out.println("Brak dostępu do pliku " + srcFile.getName() + " !");
-                System.exit(1);
-            }
-            if (dstFile.exists() && !dstFile.canWrite()) {
-                System.out.println("Nie można nadpisać pliku " + srcFile.getName() + " !");
-                System.exit(1);
-            }
-            if (dstFile.isFile() && !dstFile.canWrite()) {
-                System.out.println("Brak wymaganych uprawnień do zapisu pliku " + dstFile.getName() + " !");
-                System.exit(1);
-            }
-            if (dstFile.isDirectory()) {
-                if(!dstFile.canWrite()) {
-                    System.out.println("Brak uprawnień do katalogu " + dstFile.getName() + " !");
-                    System.exit(1);
-                }
-                dstFile = new File(Paths.get(dstFile.getName()) + "/" + srcFile.getName());
-            }
-
-            try {
-                Files.copy(srcFile.toPath(), dstFile.toPath(), REPLACE_EXISTING);
-            }
-            catch (IOException e) {
-                System.out.println("Błąd związany z: " + e.getMessage());
-                System.exit(1);
-            }
+        if(isC) {
+            System.out.println("znaków: "  + result[1]);
         }
+        if(isW) {
+            System.out.println("słów: "  + result[2]);
+        }
+        if(!isL && !isC && !isW) {
+            System.out.println("wierszy: "  + result[0]);
+            System.out.println("znaków: "  + result[1]);
+            System.out.println("słów: "  + result[2]);
+        }
+
     }
+
 }
