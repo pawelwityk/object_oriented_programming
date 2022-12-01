9f9d172d5af10bdaea3dc82d002263a2846541ab
pwityk <pawelwityk@student.agh.edu.pl>
2022-12-01 17:12:33 +0100

    added lab6

--- src/Lab5/DrawSquare.java
+++ src/Lab5/DrawSquare.java
@@ -1,16 +1,34 @@
 package Lab5;
 
-public class DrawSquare {
-    private static void drawSquare(int length) {
-        for (int i = 0; i < length; i++) {
-            System.out.print("#");
-        }
-        for (int i = 0; i < length; i++) {
-            System.out.print("\n#");
-        }
-    }
+import java.util.Scanner;
+import java.util.InputMismatchException;
 
+public class DrawSquare {
+    private static void printSquare(int size) {
+        if (size == 1) {
+            System.out.println("#");
+            System.exit(0);
+        }
+        System.out.println("#".repeat(size));
+        for(int i = 0; i < size - 2; i++) {
+            System.out.printf("#%"+(size-1)+"c\n",'#');
+        }
+        System.out.println("#".repeat(size));
+    }
     public static void main(String[] args) {
-        drawSquare(Integer.parseInt(args[0]));
+        Scanner scanner = new Scanner(System.in);
+        int size = 0;
+        System.out.println("Podaj rozmiar kwadratu: ");
+        try {
+            size = scanner.nextInt();
+        }
+        catch(InputMismatchException e) {
+            System.out.println("Błąd ! Wprowadź poprawną liczbę.");
+            System.exit(1);
+        }
+        if (size <= 0) {
+            System.out.println("Błąd ! Wprowadź liczbę większą od zera.");
+        }
+        printSquare(size);
     }
 }
